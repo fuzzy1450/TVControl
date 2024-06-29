f4e3b9afc4a51399e1078219fd4b808e5b2b8ba2
fuzzy1450 <ievank@me.com>
2024-06-29 19:39:24 -0400

    Implemented toString()

--- WebServer/src/TimeElapsed.js
+++ WebServer/src/TimeElapsed.js
@@ -1,4 +1,12 @@
 class TimeElapsed {
+	// This is a timer, 
+	// It tracks the elapsed time between it's creation and the last UpdateTimer() call
+	// UpdateTimer() will be done automatically only once, and only if it has not been called already
+	// GetTime() will call UpdateTimer() if UpdateTimer() has not been called beforebegin
+	// casting this timer to a string includes a GetTime() call
+	
+	
+	
 	constructor(){
 		this.startTime=performance.now()
 		this.endTime=-1
@@ -10,10 +18,33 @@ class TimeElapsed {
 	}
 	
 	GetTime(){
-		if(endTime != -1)}{
+		if(endTime != -1){
 			return UpdateTimer()
 		}
 		return this.endTime-this.startTime
+	}
+	
+	toString(){
+		let T = this.GetTime()
+		if(T < 1000) {
+			ms = T.toFixed(3)
+			return `${ms}ms`
+			
+		} else if(T < 60000) {
+			s = (T/1000).toFixed(3)
+			return `${s}s`
+			
+		} else if(T < 3600000) {
+			M = (T/60000).toFixed(0)
+			s = ((T - (M * 60000))/1000).toFixed(3)
+			return `${M}m:${s}s`
+			
+		} else {
+			H = (T/3600000).toFixed(0)
+			M = ((T - (H*3600000))/60000).toFixed(0)
+			s = ((T - (M * 60000))/1000).toFixed(3)
+			return `${H}H:${M}m:${s}s`
+		}
 	}
 }
 
